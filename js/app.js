be863c34cf6e6f587c515b224bd0ab675a7a4b37
SM <sean.martin@gmail.com>
2017-09-05 21:39:17 -0500

    add eventlistener to each card/li

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+/*jshint esversion: 6 */
+
 /*
  * Create a list that holds all of your cards
+ * note to self: classes: card, match,open show
  */
 const cardDeck = [
     "fa fa-diamond",
@@ -41,6 +44,86 @@ function shuffle(array) {
     return array;
 }
 
+
+// function createDeckHTML(deck) {
+//     let cards = '';
+//     deck.forEach((card) => {
+//         cards += `<li class="card">`;
+//         cards += `<i class="${card}"></i>`;
+//         cards += `</li>`;
+//     });
+//     return cards;
+// }
+function createDeckHTML(cards) {
+    // let cards = '';
+    let deck = document.querySelector(".deck");
+    
+    cards.forEach((card) => {
+        let li = document.createElement("li");
+        li.addEventListener("click", clickCard);
+        li.className = "card";
+        let i = document.createElement("i");
+        i.className = card;
+        deck.appendChild(li).appendChild(i);
+    });
+    // return cards;
+}
+createDeckHTML(shuffle(cardDeck));
+// let deckHTML = document.querySelector(".deck");
+// deckHTML.innerHTML = createDeckHTML(shuffle(cardDeck));
+
+// set up event listener for card
+// * set up the event listener for a card.If a card is clicked : * -display the card 's symbol (put this functionality in another function that you call from this one' +
+//     ')'
+function clickCard() {
+    console.log("I've been clicked.");
+}
+
+function displaySymbol() {
+
+}
+
+// * -add the card to a * list * of "open" cards(put this functionality in another function that you call from this one)
+function addCard() {
+    
+}
+
+function removeCard() {
+
+}
+
+// * -if the list already has another card,
+// check to see if the two cards match * + if the cards do 
+//     match,
+//     lock the cards in the open position(put this functionality in another function that you call from this one)
+
+// if the cards do 
+//  not match, remove the cards from the list and hide the card 's symbol
+// (put this functionality in another function that you call from this one' +
+// 
+function cardsMatch() {
+    //lockOpen();
+}
+
+function lockOpen() {
+
+}
+
+// increment the move counter and display it on the page(put this functionality in another function that you call from this one)
+
+function addMove() {
+
+}
+
+function displayMoves() {
+
+}
+
+// if all cards have matched,
+// display a message with the final score(put this functionality in another function that you call from this one)
+function gameOver() {
+
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
